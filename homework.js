2048a8507938c60029f5c388a5a0c0dec69e6526
Anastasiia Ivanova <aivanova569@bigmir.net>
2019-03-14 17:08:00 +0200

    homework 13

--- homework.js
+++ homework.js
@@ -1,2 +1,9 @@
 // -------------------------- Home work --------------------------
-// -------------------------- ФИО --------------------------
+// ----------------------- Ivanova Anastasiia --------------------
+$(function() {
+    $('.dropdown li').hover(function() {
+        $(this).children('ul').stop(false, true).fadeIn(300);
+    }, function() {
+        $(this).children('ul').stop(false, true).fadeOut(300);
+    });
+});
